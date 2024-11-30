792ac1d2bd14a4b42323d21accb8bf1cc371652e
Jahamars <146427118+Jahamars@users.noreply.github.com>
2024-11-30 15:48:24 +0500

    1

--- config/nvim/nvim.lua
+++ config/nvim/nvim.lua
@@ -42,18 +42,23 @@ vim.opt.termguicolors = true
 -- или для совместимости с vim-plug
 vim.cmd([[
 call plug#begin('~/.vim/plugged')
-
 Plug 'morhetz/gruvbox'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-buffer'
 Plug 'preservim/vim-markdown'
 Plug 'numToStr/Comment.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-lua/plenary.nvim'
 Plug 'mg979/vim-visual-multi'
+Plug 'neovim/nvim-lspconfig'
+Plug 'neovim/nvim-lspconfig' " LSP для настройки clangd
+Plug 'hrsh7th/cmp-nvim-lsp'  " Источник для LSP
+Plug 'hrsh7th/cmp-path'      " Источник для путей
 call plug#end()
 ]])
 
--- Plug 'nvim-telescope/telescope.nvim'
--- Plug 'nvim-lua/plenary.nvim'
--- Plug 'hrsh7th/nvim-cmp'
--- Plug 'hrsh7th/cmp-buffer'
+
+--Plug 'L3MON4D3/LuaSnip'      " Сниппеты
 
 vim.cmd("syntax enable")        -- Включение подсветки синтаксиса
 vim.cmd("set background=dark") -- Или 'light', если нужно
@@ -65,20 +70,20 @@ vim.api.nvim_set_keymap('v', '<Tab>', '>gv', { noremap = true, silent = true })
 -- Визуальный режим: Shift-Tab для сдвига влево
 vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
 
--- -- Настройка nvim-cmp
--- local cmp = require'cmp'
--- 
--- cmp.setup({
-  -- sources = {
-    -- { name = 'buffer' }, -- Предлагает завершение из текущего буфера
-  -- },
-  -- mapping = {
-    -- ['<C-n>'] = cmp.mapping.select_next_item(), -- Перебор вниз
-    -- ['<C-p>'] = cmp.mapping.select_prev_item(), -- Перебор вверх
-    -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Выбор завершения
-  -- },
--- })
--- 
+-- Настройка nvim-cmp
+local cmp = require'cmp'
+
+cmp.setup({
+  sources = {
+    { name = 'buffer' }, -- Предлагает завершение из текущего буфера
+  },
+  mapping = {
+    ['<C-n>'] = cmp.mapping.select_next_item(), -- Перебор вниз
+    ['<C-p>'] = cmp.mapping.select_prev_item(), -- Перебор вверх
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Выбор завершения
+  },
+})
+
 
 vim.keymap.set('n', 'd', '"_d', { noremap = true })
 vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
@@ -107,48 +112,71 @@ vim.api.nvim_create_autocmd("VimEnter", {
       	"", 
       	"", 
       	"", 
+        "				   •  •     ┓•    ",
+        "				┏┳┓┓┏┓┓┏┳┓┏┓┃┓┏┏┳┓",
+        "				┛┗┗┗┛┗┗┛┗┗┗┻┗┗┛┛┗┗",
         "",
-        -- "				   •  •     ┓•    ",
-        -- "				┏┳┓┓┏┓┓┏┳┓┏┓┃┓┏┏┳┓",
-        -- "				┛┗┗┗┛┗┗┛┗┗┗┻┗┗┛┛┗┗",
-----------------------------------------------------------
-        "				 •",
-        "				┓┏┓┏┳┓",
-        "				┗┛┗┛┗┗",
-
-----------------------------------------------------------
-
-                                     
-                                     
-                                     
--- "			             t                        ",
--- "			             Ej             ..       :",
--- "			 t      .DD. E#,           ,W,     .Et",
--- "			 EK:   ,WK.  E#t          t##,    ,W#t",
--- "			 E#t  i#D    E#t         L###,   j###t",
--- "			 E#t j#f     E#t       .E#j##,  G#fE#t",
--- "			 E#tL#i      E#t      ;WW; ##,:K#i E#t",
--- "			 E#WW,       E#t     j#E.  ##f#W,  E#t",
--- "			 E#K:        E#t   .D#L    ###K:   E#t",
--- "			 ED.         E#t  :K#t     ##D.    E#t",
--- "			 t           E#t  ...      #G      .. ",
--- "			              ,;.          j          ",
-
-        -- "",
-        "				[e] New File ",
+	"				[e] New File ",
+        "  				[f] Search ",
+        "  				[r] Recent files",
         "  				[q] Quit ",
       }
       vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
 
       -- Настраиваем команды для кнопок
       vim.api.nvim_buf_set_keymap(0, "n", "e", ":ene <BAR> startinsert<CR>", { noremap = true, silent = true })
+      vim.api.nvim_buf_set_keymap(0, "n", "f", ":Telescope find_files<CR>", { noremap = true, silent = true })
       vim.api.nvim_buf_set_keymap(0, "n", "q", ":qa<CR>", { noremap = true, silent = true })
+      vim.api.nvim_buf_set_keymap(0, "n", "r", ":Telescope oldfiles<CR>", { noremap = true, silent = true })
     end
   end,
 })
 
-      -- vim.api.nvim_buf_set_keymap(0, "n", "f", ":Telescope find_files<CR>", { noremap = true, silent = true })
-      -- vim.api.nvim_buf_set_keymap(0, "n", "r", ":Telescope oldfiles<CR>", { noremap = true, silent = true })
-        -- "  				[r] Recent files",
-        -- "  				[f] Search ",
 
+
+
+local cmp = require'cmp'
+cmp.setup({
+  mapping = {
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig').clangd.setup {
+  capabilities = capabilities,
+}
+
+
+vim.opt.pumheight = 10  -- Установить максимальную высоту всплывающего меню
+vim.opt.pumwidth = 50  -- Установить максимальную ширину всплывающего меню
+vim.opt.pumblend = 10  -- Сделать меню полупрозрачным для лучшего восприятия
+
+
+
+--
+--
+-- local nvim_lsp = require('lspconfig')
+--
+-- nvim_lsp.pylsp.setup({
+--   capabilities = require('cmp_nvim_lsp').default_capabilities(),
+--   settings = {
+--     pylsp = {
+--       plugins = {
+--         pycodestyle = { enabled = true, ignore = {'W391'}, maxLineLength = 88 },
+--         pyflakes = { enabled = true },
+--         pylint = { enabled = false },
+--         yapf = { enabled = false },
+--       },
+--     },
+--   },
+-- })
